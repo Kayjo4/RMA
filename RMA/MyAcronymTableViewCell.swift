96f2f93fecd46d557db93bba0213d2c9a2e75519
Seth Rininger <sethmr21@gmail.com>
2016-12-13 12:27:26 -0500

    added a couple of classes

--- RMA/MyAcronymTableViewCell.swift
+++ RMA/MyAcronymTableViewCell.swift
@@ -9,8 +9,5 @@
 import UIKit
 
 class MyAcronymTableViewCell: UITableViewCell {
-
-
     @IBOutlet weak var AcronymType: UILabel!
-
 }
---
+++ RMA/Profile.swift
@@ -0,0 +1,16 @@
+//
+//  Profile.swift
+//  RMA
+//
+//  Created by Seth Rininger on 12/13/16.
+//  Copyright © 2016 RMA. All rights reserved.
+//
+
+import Foundation
+
+class Profile: NSObject {
+    var firstName: String!
+    var lastName: String!
+    var email: String!
+    var password: String!
+}
